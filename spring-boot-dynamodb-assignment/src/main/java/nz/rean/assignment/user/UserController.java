8b1cfd9415726d0a7d80b098ccb5965c1816e834
SreeMandala <sreenettam@gmail.com>
2017-03-20 12:21:56 -0500

    Code

---
+++ spring-boot-dynamodb-assignment/src/main/java/nz/rean/assignment/user/UserController.java
@@ -0,0 +1,87 @@
+package nz.rean.assignment.user;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/v1")
+public class UserController {
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
+  @Autowired
+  private UserService service;
+
+  @RequestMapping(path = "/user", method = RequestMethod.GET)
+  public ResponseEntity<List<User>> list() {
+
+    log.trace("Entering list()");
+    List<User> users = service.list();
+    if (users.isEmpty()) {
+      return new ResponseEntity<>(NO_CONTENT);
+    }
+    return new ResponseEntity<>(users, OK);
+  }
+
+  @RequestMapping(path = "/user/{emailId}", method = RequestMethod.GET)
+  public ResponseEntity<User> read(@PathVariable String emailId) {
+
+    log.trace("Entering read() with {}", emailId);
+    return service.read(emailId)
+        .map(user -> new ResponseEntity<>(user, OK))
+        .orElse(new ResponseEntity<>(NOT_FOUND));
+  }
+
+  @RequestMapping(path = "/user", method = RequestMethod.POST)
+  public ResponseEntity<User> create(@RequestBody @Valid User user) {
+
+    log.trace("Entering create() with {}", user);
+    return service.create(user)
+        .map(newUserData -> new ResponseEntity<>(newUserData, CREATED))
+        .orElse(new ResponseEntity<>(CONFLICT));
+  }
+
+  @RequestMapping(path = "/user/{emailId}", method = RequestMethod.PUT)
+  public ResponseEntity<User> put(@PathVariable String emailId, @RequestBody User user) {
+
+    log.trace("Entering put() with {}, {}", emailId, user);
+    return service.replace(user.withEmailId(emailId))
+        .map(newUserData -> new ResponseEntity<>(newUserData, OK))
+        .orElse(new ResponseEntity<>(NOT_FOUND));
+  }
+
+  @RequestMapping(path = "/user/{emailId}", method = RequestMethod.PATCH)
+  public ResponseEntity<User> patch(@PathVariable String emailId, @RequestBody User user) {
+
+    log.trace("Entering patch() with {}, {}", emailId, user);
+    return service.update(user.withEmailId(emailId))
+        .map(newUserData -> new ResponseEntity<>(newUserData, OK))
+        .orElse(new ResponseEntity<>(NOT_FOUND));
+  }
+
+  @RequestMapping(path = "/user/{emailId}", method = RequestMethod.DELETE)
+  public ResponseEntity<Void> delete(@PathVariable String emailId) {
+
+    log.trace("Entering delete() with {}", emailId);
+    return service.delete(emailId) ?
+        new ResponseEntity<>(NO_CONTENT) :
+        new ResponseEntity<>(NOT_FOUND);
+  }
+}
---
+++ spring-boot-dynamodb-assignment/src/main/java/nz/rean/assignment/user/UserService.java
@@ -0,0 +1,92 @@
+package nz.rean.assignment.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.amazonaws.util.StringUtils.isNullOrEmpty;
+
+@Service
+public class UserService {
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
+  @Autowired
+  private UserRepository repository;
+
+  public Optional<User> read(String emailId) {
+
+    log.trace("Entering read() with {}", emailId);
+    return repository.read(emailId);
+  }
+
+  public Optional<User> create(User User) {
+
+    log.trace("Entering create() with {}", User);
+    if (repository.read(User.getEmailId()).isPresent()) {
+      log.warn("EmaiId {} not found", User.getEmailId());
+      return Optional.empty();
+    }
+    repository.save(User);
+    return Optional.of(User);
+  }
+
+  public Optional<User> replace(User newUserData) {
+
+    log.trace("Entering replace() with {}", newUserData);
+    Optional<User> existingUser = repository.read(newUserData.getEmailId());
+    if (!existingUser.isPresent()) {
+      log.warn("User {} not found", newUserData.getEmailId());
+      return Optional.empty();
+    }
+    User user = existingUser.get();
+    user.setName(newUserData.getName());
+    user.setAge(newUserData.getAge());
+    user.setGender(newUserData.getGender());
+    repository.save(user);
+    return Optional.of(user);
+  }
+
+  public Optional<User> update(User newUserData) {
+
+    log.trace("Entering update() with {}", newUserData);
+    Optional<User> existingUser = repository.read(newUserData.getEmailId());
+    if (!existingUser.isPresent()) {
+      log.warn("User {} not found", newUserData.getEmailId());
+      return Optional.empty();
+    }
+    User user = existingUser.get();
+    if (!isNullOrEmpty(newUserData.getName())) {
+      user.setName(newUserData.getName());
+    }
+    if (!isNullOrEmpty((newUserData.getAge().toString()))) {
+      user.setAge(newUserData.getAge());
+    }
+    if (!isNullOrEmpty((newUserData.getGender().toString()))) {
+        user.setGender(newUserData.getGender());
+      }
+    repository.save(user);
+    return Optional.of(user);
+  }
+
+  public boolean delete(String emailId) {
+
+    log.trace("Entering delete() with {}", emailId);
+    if (!repository.read(emailId).isPresent()) {
+      log.warn("User {} not found", emailId);
+      return false;
+    }
+    repository.delete(emailId);
+    return true;
+  }
+
+  public List<User> list() {
+
+    log.trace("Entering list()");
+    return repository.readAll();
+  }
+}
